e295dca87f708f0bab37431ee0574d15f95bf693
Nicholas <mickyaw1995@gmail.com>
2023-04-17 11:44:08 +0300

    page works

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from pages.views import home
-from django.urls import path
+from pages.views import homePageView, loginView
+from django.urls import include, path
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('login/', loginView, name='login'),
+    path('', homePageView, name='home'),
 ]
+
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import Blog
 
-def home(request):
-    return render(request, 'home.html')
+
+def loginView(request):
+    form = AuthenticationForm()
+    return render(request, 'pages/login.html', {'form': form})
+
+def addBlogPost(request):
+    return render(request, 'pages/addBlog.html')
+
+def deleteBlogPost(request, id):
+    return render(request, 'pages/deleteBlog.html')
+
+def editBlogPost(request, id):
+    return render(request, 'pages/editBlog.html')
+
+def homePageView(request):
+    if request.user.is_authenticated:
+        user = request.user
+        blogs = Blog.objects.filter(user=user)
+        return render(request, 'pages/index.html', {'blogs': blogs})
+    else:
+        return render(request, 'pages/index.html')
+
 
 # Create your views here.
